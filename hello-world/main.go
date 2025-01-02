f831c6f5e9191030cc556fc51fca7ecc494ba860
fadlur <fadloer@gmail.com>
2025-01-02 08:05:16 +0700

    variable

--- hello-world/main.go
+++ hello-world/main.go
@@ -4,4 +4,26 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!", "how", "are", "you?")
+
+	var firstName string = "john"
+
+	var lastName string = "wick"
+
+	fmt.Printf("Halo %s %s !\n", firstName, lastName)
+
+	/*
+		Deklarasi variabel bisa juga menggunakan new. ini digunakan untuk membuat variable pointer dengan tipe data tertentu.
+	*/
+
+	name := new(string)
+
+	fmt.Println(name)
+	fmt.Println(*name)
+
+	/*
+		Deklarasi variable bisa juga menggunakan make. ini digunakan untuk:
+		- channel
+		- slice
+		- map
+	*/
 }
